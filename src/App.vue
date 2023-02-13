3b253cfcceeb6643b7189b25f8c7913b2de82917
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2023-02-13 12:28:29 +0200

    accordion display

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <app-accordion
+    v-for="post in posts"
+    :key="post.id"
+    :title="post.title"
+    :description="post.description"
+  >
+  </app-accordion>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppAccordion from "./components/AppAccordion.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "To take into account on user level data",
+          description: [
+            "Do you have one user in more than one variant of the same AB test?",
+            "Are you users actually getting the treatment we expect them to get?",
+            "Do we have enough users in our AB test? Are those users representative for the total population?",
+          ],
+        },
+        {
+          id: 2,
+          title: "To take into account on user level data",
+          description: [
+            "Do you have one user in more than one variant of the same AB test?",
+            "Are you users actually getting the treatment we expect them to get?",
+            "Do we have enough users in our AB test? Are those users representative for the total population?",
+          ],
+        },
+        {
+          id: 3,
+          title: "To take into account on user level data",
+          description: [
+            "Do you have one user in more than one variant of the same AB test?",
+            "Are you users actually getting the treatment we expect them to get?",
+            "Do we have enough users in our AB test? Are those users representative for the total population?",
+          ],
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld,
+    AppAccordion,
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 24px;
+  color: #0d2a54;
 }
 </style>
